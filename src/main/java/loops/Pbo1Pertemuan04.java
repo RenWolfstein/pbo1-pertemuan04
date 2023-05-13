7ee5f7a7a0f54c7ee685b978c1aaabd5b550cce3
RenWolfstein <mbimam03@gmail.com>
2023-05-13 16:11:27 +0800

    while

--- src/main/java/loops/Pbo1Pertemuan04.java
+++ src/main/java/loops/Pbo1Pertemuan04.java
@@ -2,6 +2,18 @@ package loops;
 public class Pbo1Pertemuan04 {
 
     public static void main(String[] args) {
-        
+         int isiGelas = 0;
+         int penuh = 225;
+         
+         System.out.println("Isi gelas sekarang : " + isiGelas + "ml");
+         System.out.println("Isi gelas ketika Penuh : " + penuh + "ml");
+         
+         while(isiGelas != penuh){
+             isiGelas++;
+             System.out.println("Sedang mengisi gelas..");
+             System.out.println("Isi gelas sekarang : " + isiGelas+ "ml");
+             
+         }
+         System.out.println("Finale : Isi gelas sekarang : " + isiGelas + "ml");
     }
 }
