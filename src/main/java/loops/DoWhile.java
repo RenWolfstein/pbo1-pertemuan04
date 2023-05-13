b40c93e1b772daa1cb0e06d1ea5a4b01c01abaa1
RenWolfstein <mbimam03@gmail.com>
2023-05-13 16:13:22 +0800

    DoWhile

---
+++ src/main/java/loops/DoWhile.java
@@ -0,0 +1,18 @@
+package loops;
+public class DoWhile {
+     public static void main(String[] args){
+         int isiGelas = 0;
+         int penuh = 225;
+         
+         System.out.println("Isi gelas sekarang : " + isiGelas + "ml");
+         System.out.println("Isi gelas ketika Penuh : " + penuh + "ml");
+         
+         do{
+             isiGelas++;
+             System.out.println("Sedang mengisi gelas..");
+             System.out.println("Isi gelas sekarang : " + isiGelas+ "ml");
+             
+         }while(isiGelas != penuh);
+         System.out.println("Finale : Isi gelas sekarang : " + isiGelas + "ml");
+    }
+}
